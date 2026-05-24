ab420a322f2ef3fb3c261e184078ddca97b10a59
pssassa <p.soin-admin@Praimltd.ru>
2026-05-24 17:46:23 +0300

    Update form, add new column

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -454,7 +454,7 @@ function RSVPFormSection() {
     
     try {
       // 👇 ВСТАВЬ СЮДА СВОЙ URL ИЗ GOOGLE APPS SCRIPT
-      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwiz-UYHoxDZh6XBIGEFWAkFc6Eavnx4pUYZVHAqYbZK93gE9SJLHyqdWQvJspMjwK07g/exec';
+      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxbioI275GArn5mwdR_QkPvOT9LDsUJ5fqcCKqgCUyR03Yuusel-S_eOlMDZDzQgv9ZUw/exec';
       
       await fetch(GOOGLE_SCRIPT_URL, {
         method: 'POST',
